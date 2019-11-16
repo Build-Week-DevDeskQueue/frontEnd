b43cd0930de104c35a8edd8dbd4fe2ac3f669116
josepaniagua <jpanjr@gmail.com>
2019-11-16 08:35:07 -0800

    added roboto

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,17 +1,37 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import LockOpenOutlinedIcon from '@material-ui/icons/LockOpenOutlined';
 
 const useStyles = makeStyles(theme => ({
   container: {
     display: 'flex',
-    flexWrap: 'wrap'
+    flexDirection: 'column',
+    marginTop: 100
+  },
+  root: {
+    padding: theme.spacing(3, 2),
+    width: '40%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 100
   },
   textField: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
     width: 200
+  },
+  button: {
+    // margin: theme.spacing(1),
+    // display: 'inline'
+    marginLeft: 350,
+    display: 'flex'
+  },
+  centered: {
+    display: 'flex',
+    justifyContent: 'center'
   }
 }));
 
@@ -19,32 +39,46 @@ const SignIn = () => {
   const classes = useStyles();
   return (
     <div>
-      <form className={classes.container} noValidate autoComplete="off">
-        <div>
-          <TextField
-            id="standard-text"
-            label="Username"
-            type="text"
-            className={classes.textField}
-            InputLabelProps={{
-              shrink: true
-            }}
-            margin="normal"
-          />
-        </div>
-        <div>
-          <TextField
-            id="standard-text"
-            label="Password"
-            type="text"
-            className={classes.textField}
-            InputLabelProps={{
-              shrink: true
-            }}
-            margin="normal"
-          />
-        </div>
-      </form>
+      <Paper className={classes.root}>
+        <form className={classes.container} noValidate autoComplete="off">
+          <div className={classes.centered}>
+            <LockOpenOutlinedIcon />
+          </div>
+          <div className={classes.centered}>
+            <TextField
+              id="standard-text"
+              label="Username"
+              type="text"
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true
+              }}
+              margin="normal"
+            />
+          </div>
+          <div className={classes.centered}>
+            <TextField
+              id="standard-text"
+              label="Password"
+              type="text"
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true
+              }}
+              margin="normal"
+            />
+          </div>
+          <div className={classes.centered}>
+            <Button
+              color="secondary"
+              variant="contained"
+              className="classes.button"
+            >
+              Submit
+            </Button>
+          </div>
+        </form>
+      </Paper>
     </div>
   );
 };
