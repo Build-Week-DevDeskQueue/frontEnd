5cdc4b7c0f458ba5be6c9372c1ab2d276ca85d64
Carlos <miklo.concepcion@gmail.com>
2019-11-19 00:46:39 -0600

    application de-bugging. Slowly implementing redux. Double check Material UI forms and redux tomorrow. export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(App)); Double check.

--- src/index.js
+++ src/index.js
@@ -6,21 +6,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-
+import { Router } from 'react-router-dom';
 // importing the redux store
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 // react-redux provider
 import { Provider } from 'react-redux';
 
 import loginReducer from './redux/reducers/loginReducer';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import ticketReducer from './redux/reducers/ticketReducer';
 import './index.css';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 // calling the default reducer to create a link.
 const rootReducer = combineReducers({ loginReducer });
-
+// ticketReducer
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 // wrapping the application in the provider to render.
@@ -34,7 +34,7 @@ ReactDOM.render(
   rootElement
 );
 
-  document.getElementById('root');
+document.getElementById('root');
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
