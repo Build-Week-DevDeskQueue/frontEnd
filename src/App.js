a5700a36e75d0605bb44538d16b44d8b4df3ba5b
Bonnie Turnbeaugh <turnbeaugh@comcast.net>
2019-11-18 17:40:38 -0800

    updated approuter to better separate components, added dependencies, tweaked file names for standard JS naming convention

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import SignIn from './Components/SignIn';
+import SignIn from './components/SignIn';
+import AppRouter from './components/AppRouter';
 
 const App = () => {
   return (
     <div>
       <SignIn />
+      <AppRouter />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,44 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
 import App from './App';
-import Dashboard from './dashboard';
-import OpenIssues from './open';
-import ResolvedIssues from './resolved';
-import NewIssue from './new';
-
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
-
 import * as serviceWorker from './serviceWorker';
 
-const routing = (
-    <Router>
-      <div>
-        <ul>
-            <li>
-            <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-            <Link to="/open">Open Issues</Link>
-            </li>
-            <li>
-            <Link to="/resolved">Resolved Issues</Link>
-            </li>
-            <li>
-            <Link to="/new">New Issue</Link>
-            </li>
-        </ul>
-        <Route exact path='/' component={Dashboard}/>
-        <Route exact path='/open' component={OpenIssues}/>
-        <Route exact path='/resolved' component={ResolvedIssues}/>
-        <Route exact path='/new' component={NewIssue}/>
-      </div>
-    </Router>
-  )
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+  document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
