3ba346ac3ada512ccb009d99a37ad0fa98bd5c72
Derrick Mei <derrick.mei.168@gmail.com>
2019-11-18 20:07:29 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
-import SignIn from "./Components/SignIn";
+import SignIn from './Components/SignIn';
+import Header from './Components/Header';
+import Content from './Components/Content';
+import Navigator from './Components/Navigator';
 
 const App = () => {
   return (
     <div>
-        <SignIn/>
+      <SignIn />
+      {/* <Header />
+      <Content />
+      <Navigator /> */}
     </div>
   );
 };
