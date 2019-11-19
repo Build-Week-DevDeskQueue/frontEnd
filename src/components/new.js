80184317b4e06e218aae2ff5dcaef142eebf9a86
Bonnie Turnbeaugh <turnbeaugh@comcast.net>
2019-11-18 18:57:27 -0800

    basic form layout completed using formik call

--- src/components/new.js
+++ src/components/new.js
@@ -1,14 +1,87 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { useFormik } from 'formik';
 
-import App from './App';
-import Dashboard from './dashboard';
-import OpenIssues from './open';
-import ResolvedIssues from './resolved';
-import NewIssue from './new';
-
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
-
-import * as serviceWorker from './serviceWorker';
-
+const formik = useFormik({
+    initialValues: {
+        owner: '',
+        date: '',
+        title: '',
+        type: 'Select',
+        description: '',
+        tried: '',
+        assigned: '',
+        resolved: '',
+    }
+    onSubmit: values => {
+        alert(JSON.stringify(values, null, 2));
+      },
+});
+return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor='owner'>{name}</label>
+      <input
+        id="name"
+        name="name"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+      <label htmlFor='date'>{name}</label>
+      <input
+        id="date"
+        name="date"
+        type="date"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+      <label htmlFor='title'>{name}</label>
+      <input
+        id="title"
+        name="title"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+<label htmlFor='type'>{name}</label>
+      <input
+        id="type"
+        name="type"
+        type="select"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+<label htmlFor='description'>{name}</label>
+      <input
+        id="description"
+        name="description"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+      <label htmlFor='tried'>{name}</label>
+      <input
+        id="tried"
+        name="tried"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+      <label htmlFor='assigned'>{name}</label>
+      <input
+        id="assigned"
+        name="assigned"
+        type="select"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+      <label htmlFor='resolved'>{name}</label>
+      <input
+        id="resolved"
+        name="resolved"
+        type="checkbox"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+      <button type="submit">Submit</button>
+    </form>
+);
