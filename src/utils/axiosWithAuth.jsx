5058a092a8e8218298aeb379e970267ebaab56d1
Carlos <miklo.concepcion@gmail.com>
2019-11-19 19:15:36 -0600

    updated code from my enviornment

--- src/utils/axiosWithAuth.jsx
+++ src/utils/axiosWithAuth.jsx
@@ -1,8 +1,8 @@
-import axiosWithAuth from 'axios';
+import axios from 'axios';
 
 export const axiosWithAuth = () => {
   const token = localStorage.getItem('token');
-
+  
   return axios.create({
     baseURL: 'https://devdesk-backend.herokuapp.com/api/auth/',
     headers: {
