6a2f889330132add7f4d2a82cf8ee47904aed39e
Bonnie Turnbeaugh <turnbeaugh@comcast.net>
2019-11-19 17:30:06 -0800

    updated new issue form to include all fields, still needs work on select fields.

--- src/Components/AppRouter.js
+++ src/Components/AppRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './Navbar.css';
+import './navbar.css';
 
 import Dashboard from './Dashboard';
 import OpenIssues from './Open';
